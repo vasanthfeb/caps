1d066e2e541a665257040e5323df9ea97b8cd231
vasanthfeb <vasanthfeb18@gmail.com>
2024-10-25 19:42:26 +0530

    first commit master

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+document.getElementById("user_input").value="";
+
+
 function upp(){
     let answer=document.getElementById("user_input").value;
-    console.log(answer);
+    answer=answer.toUpperCase();
+    document.getElementById("user_input").value=answer;
+   
+}
+function cle(){
+    document.getElementById("user_input").value="";
+}
+
+function low(){
+    let answer=document.getElementById("user_input").value;
+    answer=answer.toLowerCase();
+    document.getElementById("user_input").value=answer;
+}
+function cpy(){
+    let answer=document.getElementById("user_input").value;
+    navigator.clipboard.writeText(answer);
+    alert("Text Copied !")
 }
